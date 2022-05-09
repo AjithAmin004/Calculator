99cc7661638256fef8e9ffd7e3aba5d297850eb9
AjithAmin004 <ajithsowda33@gmail.com>
2022-05-09 18:10:57 +0530

    Done

---
+++ script.js
@@ -0,0 +1,26 @@
+let string = "";
+let btns = document.querySelectorAll('.button');
+
+for(let i=0;i<btns.length;i++){
+    btns[i].addEventListener('click',function(e){
+    //   console.log(e);
+      console.log(e.target.innerText);
+      let val = e.target.innerText;
+      if(val == '='){
+        string = eval(string)
+        let display = document.querySelector('#ans');
+        display.value=string;
+      }else if(val=='Clear'){
+        let display = document.querySelector('#ans');
+        display.value="";
+        string='';
+      }else {
+        string = string+val;
+        let display = document.querySelector('#ans');
+        display.value=string;
+        }
+      
+    })
+}
+
+
